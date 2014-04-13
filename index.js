4176631232e8b23d0b6f14a3994d9483d74133e1
Steve Elliott <steve@tegud.net>
2014-04-13 11:02:12 +0100

    installed socket io to track clients connected

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ var express = require('express');
 var http = require('http');
 var async = require('async');
 var fs = require('fs');
+var socketIo = require('socket.io');
 
 var AppServer = function(app, options) {
     var httpServer = http.createServer(app);
+    var sync = socketIo.listen(httpServer);
 
     return {
         start: function(callback) {
             httpServer.listen(options.port, function(err) {
-                callback(err, httpServer);
+                callback(err, httpServer, sync);
             });
         },
         stop: function(callback) {
@@ -21,6 +23,7 @@ var AppServer = function(app, options) {
 var server = function() {
     var app = express();
     var httpServer;
+    var sync;
 
     app.use("/static", express.static(__dirname + '/static'));
 
@@ -32,14 +35,22 @@ var server = function() {
         res.send(fs.readFileSync(__dirname + '/static/index.html', { encoding: 'utf-8' }));
     });
 
+
     return {
         start: function(options, callback) {
             httpServer = new AppServer(app, options);
 
-            async.parallel([
-                    httpServer.start
+            async.waterfall([
+                    httpServer.start,
+                    function(http, socket) {
+                        sync = socket;
+
+                        socket.sockets.on('connection', function(socket) {
+                            console.log('client connected...');
+                        });
+                    }
                 ],
-                function(err) {
+                function(err, http, socket) {
                     (callback || function() {})(err);
                 });
         },

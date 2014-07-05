d7649fce30a119130caa82f5c2820868d0640921
Steve Elliott <steve@tegud.net>
2014-07-05 07:38:16 +0100

    Building up es indexBuilder

---
+++ static/js/BADGER/elasticsearch/indexBuilder.js
@@ -0,0 +1,36 @@
+(function() {
+	'use strict';
+
+	TLRGRP.namespace('TLRGRP.BADGER.Elasticsearch');
+
+	TLRGRP.BADGER.Elasticsearch.IndexBuilder = function(options) {
+		return { 
+			buildFromTimeFrame: function(timeFrame, queryItem) {
+				var indicies = [];
+				var day = moment().utc();
+				var oldestIndexRequired;
+				var latestIndexRequired = moment(day);
+
+	 			if(timeFrame.units === 'daysAgo') {
+					var dayOffset = parseInt(timeFrame.timeFrame, 10);
+					var queryItemDayOffset = queryItem && queryItem.dayOffset ? queryItem.dayOffset : 0;
+					day.add('d', -dayOffset + queryItemDayOffset);
+
+	 				oldestIndexRequired = day;
+	 			}
+	 			else {
+	 				oldestIndexRequired = day.subtract(timeFrame.timeFrame, timeFrame.units);
+	 			}
+
+				day = oldestIndexRequired;
+
+				while(parseInt(day.format('YYYYMMDD'), 10) <= parseInt(latestIndexRequired.format('YYYYMMDD'), 10)) {
+					indicies.push(options.prefix + day.format('YYYY.MM.DD')); 
+					day.add('d', 1);
+				}
+
+				return indicies;
+			}
+		};
+	};
+})();
---
+++ test/client/tests/elasticsearch/indexBuilder.js
@@ -0,0 +1,100 @@
+(function() {
+	'use strict';
+
+	describe('Elasticsearch indexBuilder', function () {
+		var originalMoment = moment;
+		var currentDate;
+
+		beforeEach(function() {
+			currentDate = '2014-01-01 00:00:00Z';
+
+			moment = function() {
+				if(arguments.length) {
+					return originalMoment.apply(undefined, Array.prototype.slice.call(arguments, 0));
+				}
+
+				return originalMoment(currentDate);
+			};
+		});
+
+		afterEach(function() {
+			moment = originalMoment;
+		});
+
+		describe('buildFromTimeFrame', function () {
+			describe('for relative Time Frame', function () {
+				it('returns index with specified prefix and the current date.', function () {
+					currentDate = '2014-04-05 11:00:00Z';
+
+					var menu = new TLRGRP.BADGER.Elasticsearch.IndexBuilder({
+						prefix: 'logstash-'
+					});
+
+					expect(menu.buildFromTimeFrame({ timeFrame: 15, units: 'm' })[0]).to.be('logstash-2014.04.05');
+				});
+
+				it('returns index with specified prefix and the yesterdays date and todays date when time period extends outside current date.', function () {
+					currentDate = '2014-04-05 00:10:00+00:00';
+
+					var menu = new TLRGRP.BADGER.Elasticsearch.IndexBuilder({
+						prefix: 'logstash-'
+					});
+
+					var indicies = menu.buildFromTimeFrame({ timeFrame: 1, units: 'hours' });
+
+					expect(indicies[0]).to.be('logstash-2014.04.04');
+					expect(indicies[1]).to.be('logstash-2014.04.05');
+				});
+
+				it('returns index with specified prefix and the yesterdays date and todays date when time period extends outside current date.', function () {
+					currentDate = '2014-04-05 00:10:00+00:00';
+
+					var menu = new TLRGRP.BADGER.Elasticsearch.IndexBuilder({
+						prefix: 'logstash-'
+					});
+
+					var indicies = menu.buildFromTimeFrame({ timeFrame: 1, units: 'hours' });
+
+					expect(indicies[0]).to.be('logstash-2014.04.04');
+					expect(indicies[1]).to.be('logstash-2014.04.05');
+				});
+			});
+
+			describe('for daysAgo Time Frame', function () {
+				it('returns index with specified prefix and the specified date.', function () {
+					currentDate = '2014-04-05 11:00:00Z';
+
+					var menu = new TLRGRP.BADGER.Elasticsearch.IndexBuilder({
+						prefix: 'logstash-'
+					});
+
+					expect(menu.buildFromTimeFrame({ timeFrame: 1, units: 'daysAgo' })[0]).to.be('logstash-2014.04.04');
+				});
+
+				it('returns index with specified prefix and the specified date.', function () {
+					currentDate = '2014-04-05 11:00:00Z';
+
+					var menu = new TLRGRP.BADGER.Elasticsearch.IndexBuilder({
+						prefix: 'logstash-'
+					});
+
+					expect(menu.buildFromTimeFrame({ timeFrame: 1, units: 'daysAgo' })[0]).to.be('logstash-2014.04.04');
+				});
+
+				describe('and queryItem specifies off set as well', function() {
+					it('returns index with specified prefix and the specified date.', function () {
+						currentDate = '2014-04-05 11:00:00Z';
+
+						var menu = new TLRGRP.BADGER.Elasticsearch.IndexBuilder({
+							prefix: 'logstash-'
+						});
+
+						expect(menu.buildFromTimeFrame({ timeFrame: 1, units: 'daysAgo' }, { dayOffset: -1 })[0]).to.be('logstash-2014.04.03');
+					});
+				});
+
+
+			});
+		});
+	});
+})();

05134aa0ba59510e8e6f584d16d342ca1baa2538
Steve Elliott <steve@tegud.net>
2014-07-05 08:13:26 +0100

    Factored out index logic into separate module

--- test/client/tests/elasticsearch/indexBuilder.js
+++ test/client/tests/elasticsearch/indexBuilder.js
@@ -13,7 +13,7 @@
 					return originalMoment.apply(undefined, Array.prototype.slice.call(arguments, 0));
 				}
 
-				return originalMoment(currentDate);
+				return originalMoment(currentDate).zone(currentDate);
 			};
 		});
 
@@ -93,7 +93,29 @@
 					});
 				});
 
+				describe('and user is in a timezone which is ahead of UTC', function() {
+					it('returns index with previous date.', function () {
+						currentDate = '2014-04-05 11:00:00+01:00';
 
+						var menu = new TLRGRP.BADGER.Elasticsearch.IndexBuilder({
+							prefix: 'logstash-'
+						});
+
+						expect(menu.buildFromTimeFrame({ timeFrame: 0, units: 'daysAgo' })[0]).to.be('logstash-2014.04.04');
+					});
+				});
+
+				describe('and user is in a timezone which is behind UTC', function() {
+					it.only('returns index with next date.', function () {
+						currentDate = '2014-04-05 11:00:00-01:00';
+
+						var menu = new TLRGRP.BADGER.Elasticsearch.IndexBuilder({
+							prefix: 'logstash-'
+						});
+
+						expect(menu.buildFromTimeFrame({ timeFrame: 0, units: 'daysAgo' })[1]).to.be('logstash-2014.04.06');
+					});
+				});
 			});
 		});
 	});

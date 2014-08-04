b5f54530b19ad1315303e234b4758ab92f689afb
Steve Elliott <steve@tegud.net>
2014-08-04 22:03:43 +0100

    Fixing relativeMonth edge case

--- test/client/tests/Utilities/relativeMonth.js
+++ test/client/tests/Utilities/relativeMonth.js
@@ -37,18 +37,22 @@
 			expect(calculatedDate).to.eql('03 June 2014');
 		});
 
-		it('returns 1/8/2014 to 4/7/2014', function() {
-			var startDate = '1 august 2014';
-			var calculatedDate = TLRGRP.BADGER.Utilities.relativeMonth(moment(startDate), -1).format('DD MMMM YYYY');
+		var tests = {
+			'1 august 2014': '04 July 2014',
+			'2 august 2014': '05 July 2014',
+			'3 august 2014': '06 July 2014',
+			'4 august 2014': '07 July 2014',
+			'5 august 2014': '01 July 2014',
+			'6 august 2014': '02 July 2014',
+			'7 august 2014': '03 July 2014'
+		};
 
-			expect(calculatedDate).to.eql('04 July 2014');
-		});
+		_.each(tests, function(expected, input) {
+			it('correctly maps ' + input + ' to ' + expected, function() {
+				var calculatedDate = TLRGRP.BADGER.Utilities.relativeMonth(moment(input), -1).format('DD MMMM YYYY');
 
-		it('returns 4/8/2014 to 4/7/2014', function() {
-			var startDate = '4 august 2014';
-			var calculatedDate = TLRGRP.BADGER.Utilities.relativeMonth(moment(startDate), -1).format('DD MMMM YYYY');
-
-			expect(calculatedDate).to.eql('07 July 2014');
+				expect(calculatedDate).to.eql(expected);
+			});
 		});
 	});
 })();

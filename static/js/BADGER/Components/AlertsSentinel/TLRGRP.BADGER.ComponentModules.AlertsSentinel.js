3d4b58735e8fced47f1f7b297f1afdad426122df
Andy Lowry <andrew.lowry@laterooms.com>
2014-11-12 16:08:39 +0000

    Add basic support for displaying Alerts on Dashboards

---
+++ static/js/BADGER/Components/AlertsSentinel/TLRGRP.BADGER.ComponentModules.AlertsSentinel.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    TLRGRP.namespace('TLRGRP.BADGER.Dashboard.ComponentModules');
+
+    function buildViewModel(alertData) {
+        return {
+            alertState: alertData.level
+        };
+    }
+    
+    TLRGRP.BADGER.Dashboard.ComponentModules.AlertsSentinel = function () {
+        var containerElement = $('<div class="alert-info-container"></div>');
+
+        return {
+            appendTo: function(componentElement) {
+                componentElement.append(containerElement);
+            },
+            appendToLocation: function() {
+                return 'content';
+            },
+            updateStatus: function (alertData) {
+                var viewModel = buildViewModel(alertData);
+
+                containerElement.html($(Mustache.render(
+                     '<div class="alert-error hidden">'+
+                        '<div class="alert-error-text-container">'+
+                            '<h3>Warning</h3>'+
+                            '<div class="alert-error-text">'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'+
+                    '<div class="alert-info">{{}}</div>'+
+                    '<h3>{{alertState}}</h3>', viewModel)));
+            }
+        };
+    };
+})();
+

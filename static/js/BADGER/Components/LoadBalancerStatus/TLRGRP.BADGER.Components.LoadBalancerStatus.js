3dc17a0b68afb90afe9b6a8804be7f2d274b42f6
Andy Lowry <andrew.lowry@laterooms.com>
2015-01-22 11:40:44 +0000

    Reduce LB status refresh to 15 seconds

--- static/js/BADGER/Components/LoadBalancerStatus/TLRGRP.BADGER.Components.LoadBalancerStatus.js
+++ static/js/BADGER/Components/LoadBalancerStatus/TLRGRP.BADGER.Components.LoadBalancerStatus.js
@@ -36,7 +36,7 @@
             query: {
                 url: refreshServerBaseUrl + 'poolStatus?poolId=' + configuration.poolId,
             },
-            refresh: 2500,
+            refresh: 15000,
             callbacks: {
                 success: function (data) {
                     inlineError.hide();
@@ -51,7 +51,7 @@
                         inlineError.show('Cannot access health check server.');
                     }
 
-                    dataStore.setNewRefresh(10000);
+                    dataStore.setNewRefresh(30000);
                 }
             },
             mappings: [

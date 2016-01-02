a4adc50749a4d48a96906f4423741d67d7acea4e
Steve Elliott <steve@tegud.net>
2016-01-02 23:44:54 +0000

    Refactor and tier provider summaries

--- static/js/BADGER/Components/ConnectivityService/providerSummary.js
+++ static/js/BADGER/Components/ConnectivityService/providerSummary.js
@@ -5,6 +5,20 @@
 
 	var idIncrementor = 0;
 
+	var tierOrder = {
+		'Platinum Providers': 0, 
+		'Gold Providers': 1, 
+		'Silver Providers': 2, 
+		'Bronze Providers': 3
+	};
+
+	var checkStates = {
+		'0': { name: 'OK', iconClass: 'fa fa-check', priority: 0, summaryClass: 'ok' },
+		'1': { name: 'Warn', iconClass: 'fa fa-exclamation', priority: 1, summaryClass: 'warning' },
+		'2': { name: 'Critical', iconClass: 'mega-octicon octicon-flame', priority: 3, summaryClass: 'critical' },
+		'3': { name: 'Unknown', iconClass: 'fa fa-question', priority: 2, summaryClass: 'unknown' }
+	};
+
 	TLRGRP.BADGER.Dashboard.Components.ProviderSummary = function (configuration) {
         var refreshServerBaseUrl = 'http://' + configuration.host + ':' + configuration.port + '/';
         var inlineLoading = new TLRGRP.BADGER.Dashboard.ComponentModules.InlineLoading({ cssClass: 'loading-clear-bottom' });
@@ -18,7 +32,8 @@
 			+ '<span class="fa fa-exclamation connectivity-service-summary-status-indicator warning"></span>'
 			+ '<span class="mega-octicon octicon-flame connectivity-service-summary-status-indicator critical"></span>'
 		+ '</div>').appendTo($('<li />').appendTo(summary));
-    	var overallBreakdown = $('<li />').appendTo(summary);
+    	var overallBreakdown = $('<li class="connectivity-service-breakdown" />').appendTo(summary);
+    	var overallDescription = $('<li class="connectivity-service-description"><h2>Connecting...</h2>Contacting Icinga2...</li>').appendTo(summary);
     	var providerSummary = $('<li />').appendTo(summary);
 
 		var modules = [];
@@ -43,81 +58,134 @@
 			modules: modules
 		});
 
-		var tierOrder = {
-			'Platinum Providers': 0, 
-			'Gold Providers': 1, 
-			'Silver Providers': 2, 
-			'Bronze Providers': 3
-		};
-
-		var checkStates = {
-			'0': { name: 'OK', iconClass: 'fa fa-check', priority: 0, summaryClass: 'ok' },
-			'1': { name: 'Warn', iconClass: 'fa fa-exclamation', priority: 1, summaryClass: 'warning' },
-			'2': { name: 'Critical', iconClass: 'mega-octicon octicon-flame', priority: 3, summaryClass: 'critical' },
-			'3': { name: 'Unknown', iconClass: 'fa fa-question', priority: 2, summaryClass: 'unknown' }
-		};
-
 		var callbacks = {
 			success: function (data) {
 				var tiers = _.chain(data.query.results).reduce(function(allGroups, service) {
 					return allGroups.concat(service.joins.host.groups);
 				}, []).uniq().value();
 
-				var providersGroupedByTier = _.groupBy(data.query.results, function(service) {
-					var tier = _.intersection(service.joins.host.groups, tiers);
+				var groupedData = _.chain(data.query.results)
+					.groupBy(function(service) {
+						var tier = _.intersection(service.joins.host.groups, tiers);
 
-					return tier.length ? tier[0] : 'Unknown';
+						return tier.length ? tier[0] : 'Unknown';
+					})
+					.map(function(services, key) {
+						var providers = _.chain(services).groupBy(function(service) {
+	            			return service.joins.host.name;
+						}).map(function(services, provider) {
+							var ordererdStates = _.sortBy(services, function(check) {
+								return checkStates[check.attrs.last_check_result.state].priority;
+							});
+
+							return {
+								provider: provider,
+								worstCheckState: _.chain(ordererdStates).map(function(check) {
+									return check.attrs.last_check_result.state;
+								}).last().value(),
+								services: services
+							};
+						}).value();
+
+						var ordererdStates = _.sortBy(providers, function(provider) {
+							return checkStates[provider.worstCheckState].priority;
+						});
+
+						return {
+							tier: key,
+							worstCheckState: _.chain(ordererdStates).map(function(provider) {
+								return provider.worstCheckState;
+							}).last().value(),
+							providers: providers
+						};
+					})
+					.sortBy(function(tier) {
+						return tierOrder[tier.tier];
+	            	})
+					.value();
+
+				var providerCheckCounts = _.reduce(groupedData, function(allCheckCounts, tier) {
+					var tierCounts = _.reduce(tier.providers, function(allCounts, provider) {
+						if(!allCounts[provider.worstCheckState]) {
+							allCounts[provider.worstCheckState] = 0;
+						}
+
+						allCounts[provider.worstCheckState] += 1;
+						
+						return allCounts;
+					}, {});
+
+					return _.reduce(tierCounts, function(allCheckCounts, count, state) {
+						if(!allCheckCounts[state]) {
+							allCheckCounts[state] = 0;
+						}
+						
+						allCheckCounts[state] += count;
+
+						return allCheckCounts;
+					}, allCheckCounts);
+				}, {});
+
+				var checkSummaries = _.map(checkStates, function(state, key) {
+					return _.defaults({}, state, {
+						itemClass: state.name.toLowerCase(),
+						count: providerCheckCounts[key] || 0
+					});
 				});
 
-				var groupedServiceStatus = _.countBy(data.query.results, function(service) {
-					return service.attrs.last_check_result.state;
+				var ordererdStates = _.sortBy(groupedData, function(tier) {
+					return checkStates[tier.worstCheckState].priority;
 				});
 
-				overallBreakdown.html('<ul class="state-checks">' + _.map(checkStates, function(state, stateId) {
-					return '<li class="state-checks-item ' + state.name.toLowerCase() + '"><div class="state-checks-icon ' + state.iconClass + '"></div><div class="state-checks-counter">' + (groupedServiceStatus[stateId] || 0) + ' ' + state.name + '</div></li>';
-				}).join('') + '</ul>');
+				var worstState = _.chain(ordererdStates).map(function(tier) {
+					return tier.worstCheckState;
+				}).last().value();
 
-				var ordererdStates = _.sortBy(Object.keys(groupedServiceStatus), function(state) {
-					return checkStates[state].priority;
-				});
-				var worstState = _.last(ordererdStates);
+				var summaryViewModel = {
+					overallSummaryClass: (typeof worstState !== 'undefined' ? checkStates[worstState].summaryClass : 'unknown'),
+					checkSummaries: checkSummaries,
+					tiers: _.map(groupedData, function(tier) {
+						var status;
 
-				overallSummary[0].className = 'connectivity-service-status-indicator ' + (typeof worstState !== 'undefined' ? checkStates[worstState].summaryClass : 'unknown');
- 
-            	providerSummary.html('<ul>' + _.map(_.sortBy(tiers, function(tier) {
-            		return tierOrder[tier];
-            	}), function(tier) { 
-            		var providers = {};
+						if(_.every(tier.providers, function(provider) {
+							return provider.worstCheckState == 0;
+						})) {
+							status = '<div><div class="fa fa-check connectivity-service-status-tier-big-ok-indicator"></div><div class="connectivity-service-status-tier-big-ok-text">OK</div></div>';
+						}
+						else {
+							var checkSummaries = _.map(checkStates, function(state, key) {
+								return _.defaults({}, state, {
+									itemClass: state.name.toLowerCase(),
+									count: 0
+								});
+							});
 
-            		var providerList = _.chain(providersGroupedByTier[tier]).map(function(service) {
-            			if(!providers[service.joins.host.name]) {
-            				providers[service.joins.host.name] = [];
-            			}
+							status = Mustache.render('<ul class="connectivity-service-status-tier-big-ok-indicators">{{#checkSummaries}}'
+									+ '<li class="connectivity-service-status-tier-big-ok-indicator-item {{itemClass}}"><div class="connectivity-service-status-tier-big-ok-indicator-item-icon {{iconClass}}"></div><div class="connectivity-service-status-tier-big-ok-indicator-item-counter">{{count}}</div></li>'
+								+ '{{/checkSummaries}}</ul>', { checkSummaries: checkSummaries })
+						}
 
-            			providers[service.joins.host.name].push(service);
+						return {
+							emblemClass: tier.tier.split(' ')[0].toLowerCase(),
+							emblemTitle: tier.tier[0],
+							status: status
+						};
+					})
+				};
+				
+				overallSummary[0].className = 'connectivity-service-status-indicator ' + summaryViewModel.overallSummaryClass;
 
-            			return service.joins.host.name;
-            		}).uniq().value();
+				overallBreakdown.html(Mustache.render('<ul class="state-checks">' 
+					+ '{{#checkSummaries}}<li class="state-checks-item {{itemClass}}"><div class="state-checks-icon {{iconClass}}"></div><div class="state-checks-counter">{{count}} {{name}}</div></li>{{/checkSummaries}}'
+				+ '</ul>', summaryViewModel));
 
-       //      		<b>' + tier + ': </b>' + _.map(providerList, function(provider) {
-       //      			var checksGroupedByState = _.groupBy(providers[provider], function(service) {
-       //      				return service.attrs.last_check_result.state;
-       //      			});
+				overallDescription.html(Mustache.render('<h2>Providers</h2>Description of status goes here.', {}));
 
-       //      			return provider + ' (' 
-							// + (checksGroupedByState['0'] ? checksGroupedByState['0'].length : '0') + '/' 
-							// + (checksGroupedByState['1'] ? checksGroupedByState['1'].length : '0') + '/' 
-							// + (checksGroupedByState['2'] ? checksGroupedByState['2'].length : '0') + '/'
-							// + (checksGroupedByState['3'] ? checksGroupedByState['3'].length : '0')
-       //  				+ ')';
-       //      		}).join(', ') + '
-
-            		return '<li></li>';
-        		}).join('') 
-				// + (providersGroupedByTier['Unknown'] ? '<li><b>Unknown: </b>' + _.chain(providersGroupedByTier['Unknown']).map(function(service) {
-				// 	return service.joins.host.name;
-				// }).uniq().value().join(', ') + '</li>' : '')
-				+ '</ul>');
+				providerSummary.html(Mustache.render('<ul class="connectivity-service-summary-tier-list">'
+					+ '{{#tiers}}'
+						+ '<li class="connectivity-service-summary-tier-item"><div class="connectivity-service-summary-tier-emblem {{emblemClass}}">{{emblemTitle}}</div>{{{status}}}</li>'
+					+ '{{/tiers}}'
+				+ '</ul>', summaryViewModel));
             },
             error: function (errorInfo) {
             }
